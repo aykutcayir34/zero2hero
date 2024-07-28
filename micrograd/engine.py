1a4f584cb161b07c29d886756341526b1935d680
aykutcayir34 <aykut.cayir@khas.edu.tr>
2024-07-28 15:55:56 +0300

    backward implementation is added

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -19,15 +19,33 @@ class Value:
         self.prev = set(_children)
         self._op = _op
         self.label = label
+        self.grad = 0.0
+        self._backward = lambda: None 
     def __repr__(self):
         return f"Value(data={self.data})"
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+
+        out._backward = _backward
         return out
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad
+        out._backward = _backward
         return out
-    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Value(t, (self,), 'tanh')
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+        out._backward = _backward 
+        return out
 # %%
 a = Value(2.0, label='a')
 b = Value(-3.0, label='b')
@@ -56,7 +74,7 @@ def draw_dot(root):
     dot = Digraph(format="svg", graph_attr={'rankdir': 'LR'})
     for n in nodes:
         uid = str(id(n))
-        dot.node(name=uid, label="{%s | data %.4f}" % (n.label, n.data), shape='record')
+        dot.node(name=uid, label="{%s | data %.4f | grad %.4f}" % (n.label, n.data, n.grad), shape='record')
         if n._op:
             dot.node(name=uid + n._op, label=n._op)
             dot.edge(uid + n._op, uid)
@@ -65,4 +83,31 @@ def draw_dot(root):
     return dot
 # %%
 draw_dot(L)
+# %%
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+w1 = Value(-3.0, label='w1')
+w2 = Value(1.0, label='w2')
+b = Value(6.7, label='b')
+x1w1 = x1*w1; x1w1.label='x1w1'
+x2w2 = x2*w2; x2w2.label='x2w2'
+x1w1x2w2 = x1w1+x2w2; x1w1x2w2.label='x1w1 + x2w2'
+n = x1w1x2w2+b; n.label='n'
+o = n.tanh(); o.label='o'
+draw_dot(o)
+# %%
+o.grad = 1.0
+o._backward()
+# %%
+draw_dot(o)
+# %%
+n._backward()
+# %%
+b._backward()
+# %%
+x1w1x2w2._backward()
+# %%
+x2w2._backward()
+# %%
+x1w1._backward()
 # %%
