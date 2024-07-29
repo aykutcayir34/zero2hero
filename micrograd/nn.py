e8b2cffc65b6058b38162454aac787e84221b1d8
aykutcayir34 <aykut.cayir@khas.edu.tr>
2024-07-29 17:07:39 +0300

    MLP has been added.

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -1,5 +1,5 @@
 #%%
-from engine import Value
+from engine import Value, draw_dot
 import random 
 
 class Neuron:
@@ -9,8 +9,35 @@ class Neuron:
 
     def __call__(self, x):
         # w * x + b
-        return 0.0
+        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
     
-x = [2.0, 3.0]
-n = Neuron(2)
+    def __call__(self, x):
+        outs = [neuron(x) for neuron in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+class MLP:
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+# %%
+x = [2.0, 3.0, -1.0]
+n = MLP(3, [4, 4, 1])
+n(x)
+# %%
+
+# %%
+draw_dot(n(x))
+# %%
+n(x).backward()
+draw_dot(n(x))
 # %%
