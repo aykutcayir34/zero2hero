2b2c4f130762eb3c139aa0acafaf3cd1974c026d
aykutcayir34 <aykut.cayir@khas.edu.tr>
2024-07-29 15:56:49 +0300

    division, subtraction, power and negation operations have been added.

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -24,6 +24,7 @@ class Value:
     def __repr__(self):
         return f"Value(data={self.data})"
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
         def _backward():
             self.grad += 1.0 * out.grad
@@ -32,12 +33,42 @@ class Value:
         out._backward = _backward
         return out
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
         out._backward = _backward
         return out
+    
+    def __rmul__(self, other):
+        return self * other
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float for now"
+        out = Value(self.data ** other, (self,), f'**{other}')
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
+        return out
+
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
+    
     def tanh(self):
         x = self.data
         t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
@@ -124,6 +155,26 @@ x2w2._backward()
 # %%
 x1w1._backward()
 # %%
+o.backward()
+draw_dot(o)
+# %%
+a = Value(2.0, label='a')
+b = Value(4.0, label='b')
+a - b
+# %%
+x1 = Value(2.0, label='x1')
+x2 = Value(0.0, label='x2')
+w1 = Value(-3.0, label='w1')
+w2 = Value(1.0, label='w2')
+b = Value(6.7, label='b')
+x1w1 = x1*w1; x1w1.label='x1w1'
+x2w2 = x2*w2; x2w2.label='x2w2'
+x1w1x2w2 = x1w1+x2w2; x1w1x2w2.label='x1w1 + x2w2'
+n = x1w1x2w2+b; n.label='n'
+#--------------------------
+#o = n.tanh(); o.label='o'
+e = (2*n).exp(); e.label='e'
+o = (e + -1) / (e + 1); o.label='o'
 o.backward()
 draw_dot(o)
 # %%
