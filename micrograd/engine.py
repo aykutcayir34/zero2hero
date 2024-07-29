d8295f24a0fa063188dac20bd89e3c6d9d447ecd
aykutcayir34 <aykut.cayir@khas.edu.tr>
2024-07-29 16:42:42 +0300

    Neuron class has been added.

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,16 +2,16 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
+#%matplotlib inline
 # %%
-def f(x):
-    return 3 * x ** 2 - 4 * x + 5
+#def f(x):
+#    return 3 * x ** 2 - 4 * x + 5
 # %%
-f(3.0)
+#f(3.0)
 # %%
-xs = np.arange(-5, 5, 0.25)
-ys = f(xs)
-plt.plot(xs, ys)
+#xs = np.arange(-5, 5, 0.25)
+#ys = f(xs)
+#plt.plot(xs, ys)
 # %%
 class Value:
     def __init__(self, data, _children=(), _op='', label=''): 
@@ -91,17 +91,17 @@ class Value:
         for node in reversed(topo):
             node._backward()
 # %%
-a = Value(2.0, label='a')
-b = Value(-3.0, label='b')
-c = Value(10.0, label='c')
-e = a*b; e.label='e'
-d = e+c; d.label='d'
-f = Value(-2.0, label='f')
-L = d*f; L.label='L'
-L
+#a = Value(2.0, label='a')
+#b = Value(-3.0, label='b')
+#c = Value(10.0, label='c')
+#e = a*b; e.label='e'
+#d = e+c; d.label='d'
+#f = Value(-2.0, label='f')
+#L = d*f; L.label='L'
+#L
 # %%
 from graphviz import Digraph
-# %%
+#%%
 def trace(root):
     nodes, edges = set(), set()
     def build(v):
@@ -126,55 +126,71 @@ def draw_dot(root):
         dot.edge(str(id(n1)), str(id(n2)) + n2._op)
     return dot
 # %%
-draw_dot(L)
+#draw_dot(L)
 # %%
-x1 = Value(2.0, label='x1')
-x2 = Value(0.0, label='x2')
-w1 = Value(-3.0, label='w1')
-w2 = Value(1.0, label='w2')
-b = Value(6.7, label='b')
-x1w1 = x1*w1; x1w1.label='x1w1'
-x2w2 = x2*w2; x2w2.label='x2w2'
-x1w1x2w2 = x1w1+x2w2; x1w1x2w2.label='x1w1 + x2w2'
-n = x1w1x2w2+b; n.label='n'
-o = n.tanh(); o.label='o'
-draw_dot(o)
+#x1 = Value(2.0, label='x1')
+#x2 = Value(0.0, label='x2')
+#w1 = Value(-3.0, label='w1')
+#w2 = Value(1.0, label='w2')
+#b = Value(6.7, label='b')
+#x1w1 = x1*w1; x1w1.label='x1w1'
+#x2w2 = x2*w2; x2w2.label='x2w2'
+#x1w1x2w2 = x1w1+x2w2; x1w1x2w2.label='x1w1 + x2w2'
+#n = x1w1x2w2+b; n.label='n'
+#o = n.tanh(); o.label='o'
+#draw_dot(o)
 # %%
-o.grad = 1.0
-o._backward()
+#o.grad = 1.0
+#o._backward()
 # %%
-draw_dot(o)
+#draw_dot(o)
 # %%
-n._backward()
+#n._backward()
 # %%
-b._backward()
+#b._backward()
 # %%
-x1w1x2w2._backward()
+#x1w1x2w2._backward()
 # %%
-x2w2._backward()
+#x2w2._backward()
 # %%
-x1w1._backward()
+#x1w1._backward()
 # %%
-o.backward()
-draw_dot(o)
+#o.backward()
+#draw_dot(o)
 # %%
-a = Value(2.0, label='a')
-b = Value(4.0, label='b')
-a - b
+#a = Value(2.0, label='a')
+#b = Value(4.0, label='b')
+#a - b
 # %%
-x1 = Value(2.0, label='x1')
-x2 = Value(0.0, label='x2')
-w1 = Value(-3.0, label='w1')
-w2 = Value(1.0, label='w2')
-b = Value(6.7, label='b')
-x1w1 = x1*w1; x1w1.label='x1w1'
-x2w2 = x2*w2; x2w2.label='x2w2'
-x1w1x2w2 = x1w1+x2w2; x1w1x2w2.label='x1w1 + x2w2'
-n = x1w1x2w2+b; n.label='n'
+#x1 = Value(2.0, label='x1')
+#x2 = Value(0.0, label='x2')
+#w1 = Value(-3.0, label='w1')
+#w2 = Value(1.0, label='w2')
+#b = Value(6.7, label='b')
+#x1w1 = x1*w1; x1w1.label='x1w1'
+#x2w2 = x2*w2; x2w2.label='x2w2'
+#x1w1x2w2 = x1w1+x2w2; x1w1x2w2.label='x1w1 + x2w2'
+#n = x1w1x2w2+b; n.label='n'
 #--------------------------
 #o = n.tanh(); o.label='o'
-e = (2*n).exp(); e.label='e'
-o = (e + -1) / (e + 1); o.label='o'
-o.backward()
-draw_dot(o)
+#e = (2*n).exp(); e.label='e'
+#o = (e + -1) / (e + 1); o.label='o'
+#o.backward()
+#draw_dot(o)
+# %%
+#import torch
+#x1 = torch.Tensor([2.0]).double(); x1.requires_grad = True
+#x2 = torch.Tensor([0.0]).double(); x2.requires_grad = True
+#w1 = torch.Tensor([-3.0]).double(); w1.requires_grad = True
+#w2 = torch.Tensor([1.0]).double(); w2.requires_grad = True
+#b = torch.Tensor([6.8813735870195432]).double(); b.requires_grad = True
+#n = x1*w1 + x2*w2 + b
+#o = torch.tanh(n)
+#print(o.data.item())
+#o.backward()
+#print('---')
+#print(x2.grad.item())
+#print(w2.grad.item())
+#print(x1.grad.item())
+#print(w1.grad.item())
 # %%
---
+++ micrograd/nn.py
@@ -0,0 +1,16 @@
+#%%
+from engine import Value
+import random 
+
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1, 1))
+
+    def __call__(self, x):
+        # w * x + b
+        return 0.0
+    
+x = [2.0, 3.0]
+n = Neuron(2)
+# %%
